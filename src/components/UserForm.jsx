116ce993040e4685990898bbdf3aad163c9ee499
acabezar <uzumaki1995>
2020-12-02 14:33:32 +0100

    changes

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 
 export default class UserForm extends React.Component {
-
-    createUser = (e) => {
-        e.preventDefault();
-
-        const name = document.getElementById('name').value;
-        const years = document.getElementById('years').value;
-        const role = document.getElementById('role').value;
-
-        this.props.fnAddUser({name,years,role});
+    state = {}
+    
+    handleChange = ($event) => {
+        this.setState({ [$event.target.name]: $event.target.value });
     }
     
-    render(){
+    createUser = ($event) => {
+        $event.preventDefault();
+
+        // const name = document.getElementById('name').value;
+        // const years = document.getElementById('years').value;
+        // const role = document.getElementById('role').value;
+
+        // this.props.fnAddUser({ name, years, role });
+        this.props.fnAddUser(this.state);
+    }
+
+    render() {
         return (
-           <form onSubmit={this.createUser}>
+            <form onSubmit={this.createUser}>
 
-            <label htmlFor="name">Name</label>
-            <input id="name" type="text"/>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" name="name" onChange={this.handleChange}/>
 
-            <label htmlFor="years">Years</label>
-            <input id="years" type="number"/>
+                <label htmlFor="years">Years</label>
+                <input id="years" type="number" name="years" onChange={this.handleChange} />
 
-            <label htmlFor="role">Role</label>
-            <input id="role" type="text"/>
 
-            <input type="submit" value="Guardar"/>
+                <label htmlFor="role">Role</label>
+                <select name="role" id="role" onChange={this.handleChange}>
+                    <option value=""></option>
+                    <option value="Admin">Admin</option>
+                    <option value="User">User</option>
+                </select>
 
-           </form>
+                <input type="submit" value="Guardar" />
+
+            </form>
         )
     }
 }
